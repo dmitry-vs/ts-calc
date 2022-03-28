4b387a16e641755e7242337793fd9178b206322c
dmitry-vs <dmitryselin@inbox.ru>
2022-03-28 21:53:03 +0300

    (parser) - добавлено распознавание унарных операторов

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,3 +1,5 @@
+import { parseInt } from 'lodash';
+
 export type ParsedInput = Array<number | string>;
 
 export const parseInput = (input: string): ParsedInput | null => {
@@ -5,10 +7,32 @@ export const parseInput = (input: string): ParsedInput | null => {
   const inputParts = input.split(' ');
 
   for (const part of inputParts) {
-    if (!part || part.trim() !== part) return null;
-    const partNum = parseInt(part);
-    result.push(isNaN(partNum) ? part : partNum);
+    const parsedPart = parseInputPart(part);
+    if (parsedPart === null) return null;
+    result.push(...parsedPart);
   }
 
   return result;
+};
+
+export const parseInputPart = (inputPart: string): ParsedInput | null => {
+  // single number
+  if (/^-?\d+$/.test(inputPart)) return [parseInt(inputPart)];
+
+  // binary operator
+  if (/^[+-/*^]$/.test(inputPart)) return [inputPart];
+
+  // unary operator
+  const unaryOperatorMatches = inputPart.match(/^(-?\d+)(\*{2}|!)$/);
+  if (unaryOperatorMatches !== null)
+    return [parseInt(unaryOperatorMatches[1]), unaryOperatorMatches[2]];
+
+  // unary function
+  const unaryFunctionMatches = inputPart.match(
+    /^(fib|sin|cos|tan)\((-?\d+)\)$/
+  );
+  if (unaryFunctionMatches !== null)
+    return [parseInt(unaryFunctionMatches[2]), unaryFunctionMatches[1]];
+
+  return null;
 };
